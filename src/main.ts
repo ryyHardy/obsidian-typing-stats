11e9b9cb623b7adb83e5b9b262f26aa8cd98285a
Ryan Hardy <ryyhardy@gmail.com>
2026-06-29 15:36:36 -0400

    Added a statusbar indicator showing edits

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
-import {
-	Editor,
-	MarkdownView,
-	MarkdownFileInfo,
-	Modal,
-	Notice,
-	Plugin,
-} from 'obsidian';
+import { Plugin } from 'obsidian';
 import {
 	KeyStatsSettings,
 	DEFAULT_SETTINGS,
 	KeyStatsSettingTab,
 } from './settings';
 
+import { EditorView } from '@codemirror/view';
+
 export default class KeyStats extends Plugin {
+	private statusBarItemEl!: HTMLElement;
 	settings!: KeyStatsSettings;
 
 	async onload() {
@@ -21,6 +17,44 @@ export default class KeyStats extends Plugin {
 		// Set up commands here if needed
 
 		this.addSettingTab(new KeyStatsSettingTab(this.app, this));
+
+		this.statusBarItemEl = this.addStatusBarItem().createEl('span', {
+			text: 'Stats: Ready',
+		});
+
+		this.registerEditorExtension(
+			EditorView.updateListener.of((update) => {
+				if (!update.docChanged) return;
+
+				let totalAdded = 0;
+				let totalDeleted = 0;
+
+				for (const tr of update.transactions) {
+					if (!tr.docChanged) continue;
+
+					tr.changes.iterChanges(
+						(fromA, toA, fromB, toB, inserted) => {
+							const deletedStr = tr.startState.doc.sliceString(
+								fromA,
+								toA,
+							);
+							const addedStr = inserted.toString();
+
+							totalAdded += addedStr.length;
+							totalDeleted += deletedStr.length;
+						},
+					);
+				}
+
+				if (totalAdded > 0 || totalDeleted > 0) {
+					window.requestAnimationFrame(() => {
+						this.statusBarItemEl.setText(
+							`+${totalAdded} chrs | -${totalDeleted} chrs`,
+						);
+					});
+				}
+			}),
+		);
 	}
 
 	onunload() {}
